7b5dbff8679a3495983b393978f0725731d10148
Brandon Lee <brandonlee781@gmail.com>
2022-02-10 19:46:34 -0600

    This makes way more sense

--- src/components/ScheduleTable.vue
+++ src/components/ScheduleTable.vue
@@ -2,11 +2,11 @@
   <BaseTable :headers="headers">
     <tr>
       <Data>{{ format(new Date('2022-02-11T18:30:00+0000'), 'h:mmaaa') }}</Data>
-      <Data v-for="n in (headers.length - 1)" :key="`start-${n}`">Broadcast Start</Data>
+      <Data :colspan="headers.length - 1" class="text-left xl:text-center">Broadcast Start</Data>
     </tr>
     <tr>
       <Data>{{ format(new Date('2022-02-11T18:45:00+0000'), 'h:mmaaa') }}</Data>
-      <Data v-for="n in (headers.length - 1)" :key="`pre-${n}`">Anaheim Preshow</Data>
+      <Data :colspan="headers.length - 1" class="text-left xl:text-center">Anaheim Preshow</Data>
     </tr>
     <MatchRow
       v-for="(time, index) in timeslots"
@@ -16,7 +16,7 @@
     />
     <tr>
       <Data>{{ format(new Date('2022-03-11T02:45:00+0000'), 'h:mmaaa') }}</Data>
-      <Data v-for="n in (headers.length - 1)" :key="`pre-${n}`">Broadcast Ends</Data>
+      <Data :colspan="headers.length - 1" class="text-left xl:text-center">Broadcast Ends</Data>
     </tr>
   </BaseTable>
 </template>
