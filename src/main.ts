282e31f086dbcf8abf826851774ca0009e88dbe5
Brandon Lee <brandonlee781@gmail.com>
2022-09-16 15:57:07 -0500

    install pinia

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
 import 'virtual:windi.css'
+import { useUiStore, darkModeKey } from './store/ui'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(pinia)
+app.mount('#app')
+
+const uiStore = useUiStore(pinia)
+
+if (
+  (!localStorage[darkModeKey] &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches) ||
+  localStorage[darkModeKey] === '1'
+) {
+  uiStore.setDarkMode(true)
+}
