bf7046523af7c61f13c0426a7f1d3d2c5f0c2b6f
Brandon Lee <brandonlee781@gmail.com>
2022-09-19 13:38:14 -0500

    Ignore error

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
+// eslint-disable-next-line import/no-unresolved
 import Components from 'unplugin-vue-components/vite'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
