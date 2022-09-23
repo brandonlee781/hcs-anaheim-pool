b35d26f310f6495445e8980b6c11e3d391b794c4
Brandon Lee <brandonlee781@gmail.com>
2022-09-23 12:30:31 -0500

    Reload prompt z index

--- src/components/ReloadPrompt.vue
+++ src/components/ReloadPrompt.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { useUiStore } from '@/store/ui'
 import { useRegisterSW } from 'virtual:pwa-register/vue'
 const intervalMS = 15 * 60 * 1000
 
@@ -15,15 +16,18 @@ const close = async () => {
   offlineReady.value = false
   needRefresh.value = false
 }
+
+const uiStore = useUiStore()
 </script>
 
 <template>
   <div
-    v-if="offlineReady || needRefresh"
-    class="pwa-toast bg-gray-700 rounded-lg text-white"
+    v-if="true"
+    class="pwa-toast rounded-lg z-10 fixed bottom-4 right-4 border-1 border-gray-500 shadow-md text-left m-4 p-3"
+    :class="[uiStore.tableHeadStyle]"
     role="alert"
   >
-    <div class="message">
+    <div class="mb-2">
       <span v-if="offlineReady"> App ready to work offline </span>
       <span v-else>
         New content available, click on reload button to update.
@@ -35,19 +39,6 @@ const close = async () => {
 </template>
 
 <style>
-.pwa-toast {
-  position: fixed;
-  right: 0;
-  bottom: 0;
-  margin: 16px;
-  padding: 12px;
-  z-index: 1;
-  text-align: left;
-  border: 1px solid #8885;
-}
-.pwa-toast .message {
-  margin-bottom: 8px;
-}
 .pwa-toast button {
   border: 1px solid #8885;
   outline: none;
