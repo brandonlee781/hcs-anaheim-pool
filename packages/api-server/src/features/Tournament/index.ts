a5bb0cee3972b13b6f51c1ed8a53eda1c1dd884b
Brandon Lee <brandonlee781@gmail.com>
2022-10-26 16:22:28 -0500

    Removed workspaces and down to just a single project repo

--- packages/api-server/src/features/Tournament/index.ts
+++
@@ -1,3 +0,0 @@
-export { tournamentRouter, TournamentRouter } from './routes';
-export type { Tournament, TournamentDay } from './types'
-
--- packages/api-server/src/router/app.ts
+++
@@ -1,8 +0,0 @@
-import { tournamentRouter } from '@/features/Tournament';
-import { router } from '@/trpc'
-
-export const appRouter = router({
-  tournament: tournamentRouter
-})
-
-export type AppRouter = typeof appRouter;
