6e43f171038b8565a3eedb22ec51c9fbc32deb1b
Brandon Lee <brandonlee781@gmail.com>
2022-10-28 13:03:41 -0500

    Don't use sidebar until we need it Will need to be redesigned

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <AppProvider>
       <div className="App">
-        <div className="flex flex-row flex-nowrap h-full w-full">
-          <Sidebar />
+        <div className="flex flex-row flex-nowrap h-full w-full max-w-[1400px] mx-auto">
+          {/* <Sidebar /> */}
           <SchedulePage />
         </div>
       </div>
