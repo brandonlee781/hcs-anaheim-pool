f6fe7d1a64d6668845b3df2dec7a85bd79961a86
Brandon Lee <brandonlee781@gmail.com>
2022-10-28 13:02:27 -0500

    Also removed trpc

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,9 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { httpBatchLink } from '@trpc/client'
 
 import { HoverTeamProvider } from '@/features/teams'
 import { TournamentDayProvider } from '@/features/tournament'
-import { trpc } from '@/lib/trpc'
 
 import { ThemeProvider } from './ThemeProvider'
 
@@ -13,24 +11,14 @@ type AppProviderProps = {
 
 export const AppProvider = ({ children }: AppProviderProps) => {
   const [queryClient] = useState(() => new QueryClient())
-  const [trpcClient] = useState(() =>
-    trpc.createClient({
-      links: [
-        httpBatchLink({
-          url: 'http://localhost:8080/trpc',
-        }),
-      ],
-    })
-  )
+
   return (
-    <trpc.Provider client={trpcClient} queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>
-        <HoverTeamProvider>
-          <ThemeProvider>
-            <TournamentDayProvider>{children}</TournamentDayProvider>
-          </ThemeProvider>
-        </HoverTeamProvider>
-      </QueryClientProvider>
-    </trpc.Provider>
+    <QueryClientProvider client={queryClient}>
+      <HoverTeamProvider>
+        <ThemeProvider>
+          <TournamentDayProvider>{children}</TournamentDayProvider>
+        </ThemeProvider>
+      </HoverTeamProvider>
+    </QueryClientProvider>
   )
 }
