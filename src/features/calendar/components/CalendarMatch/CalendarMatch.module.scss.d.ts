cc5ff30a22b0af524f3db557bacbb6e8844106a3
Brandon Lee <brandonlee781@gmail.com>
2022-11-02 19:41:01 -0500

    Remove unused sass files

--- src/features/calendar/components/CalendarMatch/CalendarMatch.module.scss.d.ts
+++
@@ -1,2 +0,0 @@
-export const pulse: string
-export const pulseColor: string
--- src/features/schedule/pages/SchedulePage/SchedulePage.module.scss.d.ts
+++
@@ -1,6 +0,0 @@
-declare const classNames: {
-  readonly content: 'content'
-  readonly pools: 'pools'
-}
-export default classNames
-export type ClassNames = 'content' | 'pools'
